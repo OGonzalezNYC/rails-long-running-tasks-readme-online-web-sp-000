06e1c67f09d9fba771e3e17a1d56a2dc1276f9a5
Omar Gonzalez <ogonzaleznyc@gmail.com>
2020-02-24 03:07:42 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :customers, only: [:index]
   
+  # The below line is added.
   post 'customers/upload', to: 'customers#upload'
-  
+  # The above line is added.
 end
